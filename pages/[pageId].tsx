b5d001eb314cbc7ce50eed46c23d0b2dac5117c5
jihchi <achi@987.tw>
2022-05-07 12:32:41 -0400

    Clone https://github.com/transitive-bullshit/nextjs-notion-starter-kit.git

---
+++ pages/[pageId].tsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+import { isDev, domain } from 'lib/config'
+import { getSiteMap } from 'lib/get-site-map'
+import { resolveNotionPage } from 'lib/resolve-notion-page'
+import { NotionPage } from 'components'
+
+export const getStaticProps = async (context) => {
+  const rawPageId = context.params.pageId as string
+
+  try {
+    const props = await resolveNotionPage(domain, rawPageId)
+
+    return { props, revalidate: 10 }
+  } catch (err) {
+    console.error('page error', domain, rawPageId, err)
+
+    // we don't want to publish the error version of this page, so
+    // let next.js know explicitly that incremental SSG failed
+    throw err
+  }
+}
+
+export async function getStaticPaths() {
+  if (isDev) {
+    return {
+      paths: [],
+      fallback: true
+    }
+  }
+
+  const siteMap = await getSiteMap()
+
+  const staticPaths = {
+    paths: Object.keys(siteMap.canonicalPageMap).map((pageId) => ({
+      params: {
+        pageId
+      }
+    })),
+    // paths: [],
+    fallback: true
+  }
+
+  console.log(staticPaths.paths)
+  return staticPaths
+}
+
+export default function NotionDomainDynamicPage(props) {
+  return <NotionPage {...props} />
+}
