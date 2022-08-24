d578d2ce10bcff793b72166c4a73bc02997e994c
jihchi <achi@987.tw>
2022-08-24 12:29:04 -0400

    Upgrade astro to 1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://infofinland.tw',
-  integrations: [sitemap()]
+  integrations: [sitemap(), mdx()]
 });
